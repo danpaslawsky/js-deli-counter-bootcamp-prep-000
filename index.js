7d123d1f4774c8b42944cad474bcd268700d36f3
danpaslawsky <danpaslawsky@gmail.com>
2020-04-15 23:53:00 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+
+function takeANumber(katzDeliLine, newPerson){
+   katzDeliLine.push(newPerson);
+   return `Welcome, ${newPerson}. You are number ${katzDeliLine.length} in line.`;
+ }
+ 
+function nowServing(katzDeliLine){
+   var lineFirst = "Currently serving";
+   if (katzDeliLine.length > 0){
+     var firstPerson = `${lineFirst} ${katzDeliLine[0]}.`;
+        katzDeliLine.shift();
+        return firstPerson;
+
+   }
+   else{
+     return `There is nobody waiting to be served!`;
+ }
+}
