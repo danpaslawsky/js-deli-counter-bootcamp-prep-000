00ef4adb00b0576e446f94f26f50aa16b128f762
danpaslawsky <danpaslawsky@gmail.com>
2020-04-16 00:13:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function currentLine(line){
   var currentLine = "The line is currently: ";
   if (line.length > 0){
     var katzLine = `${currentLine} ${line}`;
-    return katzLine.shift(1);
+    return katzLine;
   }
   else{
     return `The line is currently empty.`;
